b5f941e3639ab445b12e125c4ffc309dffb89300
Riyad22750 <112902985+Riyad22750@users.noreply.github.com>
2024-04-11 15:09:22 +0600

    Add files via upload

---
+++ bkash-boom.py
@@ -0,0 +1,23 @@
+import requests
+
+logo = ("""
+ ____          ____  
+|  _   _  _   \  /   __   \|_   _| 
+|_/ | | \_|  |   \/   |    |   \ | |   
+    | |      | |\  /| |    | |\ \| |   
+   _| |_    _| |_\/_| |_  _| |_\   |_  
+  |_|  |____|\_____|
+""")
+print(logo)
+
+number = input("Enter Target Number: ")
+url = "https://cpp.bka.sh/external-services/referral/report/otp/request"
+data = {"referrerWallet": number}
+
+response = requests.post(url, json=data)
+
+if response.status_code == 200:
+    print("OTP request successful!")
+    print("Response:", response.json())
+else:
+    print("Error requesting OTP. Status code:", response.status_code)
